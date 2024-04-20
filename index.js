a2100eaa2139a7c3ced898aac36d7c373a60cd5b
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-04-20 12:49:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// CommonJS
+// const { mul, div, add, sub } = require("./utilities.js");
+
+// Module JS import
+import { mul, div, add, sub } from "./utilities.js";
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
+console.log(div(2, 3));
+console.log(mul(2, 3));
+
+console.log("Hi Hello I am a NPM Project");
---
+++ utilities.js
@@ -0,0 +1,28 @@
+// functions in this file will be used in other files
+
+const mul = (a, b) => {
+  return a * b;
+};
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+// module.exports = {
+//   add,
+//   sub,
+//   mul,
+//   div,
+// };
+
+// export default { mul };
+
+export { add, sub, div, mul };
